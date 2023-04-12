3f517347b57f6ec1105aa4eeaef0d9895fbb2f83
mandipadk <92096340+mandipadk@users.noreply.github.com>
2023-04-12 11:42:22 +0545

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Telegram-Visa-Script
-A python script to alert user upon visa slots opening by filtering telegram messages
+# tvas - telegram visa alert script
+A python script to alert user upon visa slots opening by filtering telegram messages using telethon telegram library, and also provides SMS alert feature using twilio. 
+
+Steps:
+1. do pip install -r requirements.txt
+2. add your secrets to secrets.env
+3. do python main.py
+4. if you're running the script for the first time, it will prompt you to give your phone number. add your telegram account phone number in international format.
+5. login and VOILÀ 
--- main.py
+++ main.py
@@ -1,192 +1,97 @@
 #Python scipt which uses Telethon - Telegram API to find messsages containing
-#keywords like Go, Open, Opened, etc. which indicates visa slots opening.
-#Upon finding these keywords in message it will play sound.
+#keywords mentioned in the regex indicate visa slots opening.
+#Upon finding these keywords in new messages, it will play an alert sound.
 
-#@author Rushabh Patel
+#original Rushabh Patel, modified by Mandip Adhikari
 
-#Importing modules and install required libraries, if gives an error
+#Importing modules
 from telethon import TelegramClient, events, utils
 from pygame import mixer
 import threading
 import time
 import datetime
+import re
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
 
 date = 'Unknown'
-
-#IMPORTANT : Use your api_id and api_hash by signing at 'https://my.telegram.org/auth'
-api_id = 00000
-api_hash = 'abcd0123456'
 th = None
 sound = None
 
 mixer.init()
 
 print('---------------------------------------------------------')
-print('STARTED AT', datetime.datetime.now(), 'JAI MATA JI /\\')
+print('STARTED AT', datetime.datetime.now(), 'Om Namah Shivaya Date Paiyos /\\')
 print('---------------------------------------------------------')
 print()
 print('Select sound length:')
 print(' 1. Short')
 print(' 2. Long')
-sip = input('Enter : ')
+sip = input("Choose an option: ")
 print()
 
 if(sip=='1'):
-    sound = 'C:/Users/rusha/Desktop/short_alarm.mp3' #short sound file path. NOTE: USE ONLY FORWARD SLASHES IN PATH
+    sound = "./shortalert.wav" #short sound file path. NOTE: USE ONLY FORWARD SLASHES IN PATH
 elif(sip=='2'):
-    sound = 'C:/Users/rusha/Desktop/alarm.mp3' #long sound file path
+    sound = './longalert.wav' #long sound file path
 else:
-    sound = 'C:/Users/rusha/Desktop/short_alarm.mp3' #default sound file path
+    sound = './shortalert.wav' #default sound file path
 
 def alert():
-    if('Mumbai Visa Prep & Experience' in date or 'F1 visa interview Experiences' in date):
+    if('Visa Prep & Experience' in date or 'F1 visa interview Experience' in date):
         #Not playing sound, showing alert for visa experience groups messages
         return
     print('FOUND GO AT', datetime.datetime.now(), ' | ', date)
+    print("Playing sound alert")
     mixer.music.load(sound)
     mixer.music.play()
     stop_alert()
 
 def stop_alert():
-    time.sleep(5) #Playing sound for 5 secs. You can change according to your need
+    time.sleep(8) #Playing sound for 8 secs. You can change according to your need
     mixer.music.stop()
 
+#Setting up the SMS alert client
+
+SMSclient = Client(os.getenv('account_sid', default=None), os.getenv('auth_token', default=None))
+
 #Starting client
-client = TelegramClient('session_name', api_id, api_hash)
+client = TelegramClient('session_name', os.getenv('api_id', default=None), os.getenv('api_hash', default=None))
 client.start()
 
-
 #Setting up async handlers to find pattern in messages
-
-@client.on(events.NewMessage(pattern="go+|Go+|GO+"))
+regex = r"(.|)(april|may|june|july)(\s|\,|\,\s)([1-9]).+(2023)"
+regexSecond = r"(.|)(2023)(\s|\,|\,\s)(april|may|june|july)[\s\S]([1-9])."
+@client.on(events.NewMessage())
 async def handler(event):
     global date
     chat_from = event.chat if event.chat else (await event.get_chat())
     chat_title = utils.get_display_name(chat_from)
+    newText = event.raw_text
     date = chat_title + ' : ' + event.raw_text
-    if ('logout' not in event.raw_text and 'Logout' not in event.raw_text and 'LOGOUT' not in event.raw_text):
-        th = threading.Thread(target=alert())
-        th.start()
-
-@client.on(events.NewMessage(pattern=".+go|.+Go|.+GO"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    if ('logout' not in event.raw_text and 'Logout' not in event.raw_text and 'LOGOUT' not in event.raw_text):
-        th = threading.Thread(target=alert())
-        th.start()
-
-@client.on(events.NewMessage(pattern="open+|Open+|OPEN+"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    th = threading.Thread(target=alert())
-    th.start()
-
-@client.on(events.NewMessage(pattern=".+open|.+Open|.+OPEN"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    th = threading.Thread(target=alert())
-    th.start()
-
-@client.on(events.NewMessage(pattern="opened+|Opened+|OPENED+"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    th = threading.Thread(target=alert())
-    th.start()
-
-@client.on(events.NewMessage(pattern=".+opened|.+Opened|.+OPENED"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    th = threading.Thread(target=alert())
-    th.start()
-
-@client.on(events.NewMessage(pattern="available+|Available+|AVAILABLE+"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    if('not' not in event.raw_text and 'Not' not in event.raw_text):
-        th = threading.Thread(target=alert())
-        th.start()
-
-@client.on(events.NewMessage(pattern=".+available|.+Available|.+AVAILABLE"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    if ('not' not in event.raw_text and 'Not' not in event.raw_text):
-        th = threading.Thread(target=alert())
-        th.start()
-
-@client.on(events.NewMessage(pattern="nov+|Nov+|NOV+"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    th = threading.Thread(target=alert())
-    th.start()
-
-@client.on(events.NewMessage(pattern=".+nov|.+Nov|.+NOV"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    th = threading.Thread(target=alert())
-    th.start()
-
-@client.on(events.NewMessage(pattern="dec+|Dec+|DEC+"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    th = threading.Thread(target=alert())
-    th.start()
-
-@client.on(events.NewMessage(pattern=".+dec|.+Dec|.+DEC"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    th = threading.Thread(target=alert())
-    th.start()
-
-@client.on(events.NewMessage(pattern="mar+|Mar+|MAR+"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    th = threading.Thread(target=alert())
-    th.start()
-
-@client.on(events.NewMessage(pattern=".+mar|.+Mar|.+MAR"))
-async def handler(event):
-    global date
-    chat_from = event.chat if event.chat else (await event.get_chat())
-    chat_title = utils.get_display_name(chat_from)
-    date = chat_title + ' : ' + event.raw_text
-    th = threading.Thread(target=alert())
-    th.start()
+    print("New messsage: "+ newText)
+    matches = re.finditer(regex, newText, re.MULTILINE | re.IGNORECASE)
+    matchesSecond = re.finditer(regexSecond, newText, re.MULTILINE | re.IGNORECASE)
+    print("----------------------------------")
+    for matchNum, match in enumerate(matches, start=1):
+        if match:
+            th = threading.Thread(target=alert())
+            th.start()
+            message = SMSclient.messages.create(
+                body="VISA DATE ALERT "+ date,
+                from_=os.getenv('twiliofrom', default=None), 
+                to=os.getenv('twilioto', default=None)
+)
+            print("SMS Sent:",message)
+        print("----------------------------------")
+    for matchSecond in enumerate(matchesSecond):
+        if matchSecond:
+            th = threading.Thread(target=alert())
+            th.start()
+        print("----------------------------------")
 
 
 client.run_until_disconnected() #Program will run until stopped by user
